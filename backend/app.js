908e7eeb3f735e9762d248b120d4980a4e7c4134
Adam Neef <adneef@gmail.com>
2017-11-18 08:56:16 -0700

    oauth working

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,15 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var passport = require('passport');
+var express = require('express')
+var path = require('path')
+var favicon = require('serve-favicon')
+var logger = require('morgan')
+var cookieParser = require('cookie-parser')
+var bodyParser = require('body-parser')
+var passport = require('passport')
 require('dotenv').config()
 
+var index = require('./routes/index')
+var users = require('./routes/users')
 
-var index = require('./routes/index');
-var users = require('./routes/users');
 
 var cookieSession = require('cookie-session')
 var express = require('express')
@@ -25,8 +25,8 @@ app.use(cookieSession({
 }))
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'jade')
 
 app.use(passport.initialize())
 app.use(passport.session())
@@ -45,43 +45,43 @@ passport.deserializeUser((object, done) => {
 })
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/', index);
-app.use('/users', users);
+app.use('/', index)
+app.use('/users', users)
 
-var GoogleStrategy = require('passport-google-oauth20').Strategy;
+var GoogleStrategy = require('passport-google-oauth20').Strategy
 
 passport.use(new GoogleStrategy({
   clientID: `${process.env.GOOGLE_CLIENT_ID}`,
   clientSecret: `${process.env.GOOGLE_CLIENT_SECRET}`,
-  callbackURL: "http://localhost:3000/auth/google/callback",
+  callbackURL: "http://localhost:5000/auth/google/callback",
   passReqToCallback: true
 }, function(request, accessToken, refreshToken, profile, done) {
-  console.log("good things happening passport...", accessToken, refreshToken, profile);
+  console.log("good things happening passport...", accessToken, refreshToken, profile)
 
-  return done(null, profile);
+  return done(null, profile)
 }))
 
-app.get('/auth/google', passport.authenticate('google', {scope: ['profile']}));
+app.get('/auth/google', passport.authenticate('google', {scope: ['profile']}))
 
 app.get('/auth/google/callback', passport.authenticate('google', {
   successRedirect: '/auth/google/success',
   failureRedirect: '/auth/google/failure'
-}));
+}))
 
 app.get('/auth/google/success', (req, res) => {
   res.send("successfully logged in")
-});
+})
 
 app.get('/auth/google/failure', (req, res) => {
   res.send("zoinks, failure")
-});
+})
 
 app.get('/auth/logout', (req, res) => {
   	req.logOut()
@@ -90,22 +90,22 @@ app.get('/auth/logout', (req, res) => {
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+  var err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
 
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
+  res.locals.message = err.message
   res.locals.error = req.app.get('env') === 'development'
     ? err
-    : {};
+    : {}
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+  res.status(err.status || 500)
+  res.render('error')
+})
 
-module.exports = app;
+module.exports = app
