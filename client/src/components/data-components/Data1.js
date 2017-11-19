2168f8493542f643287d086d2422c6877af93135
Brianna Greene <brianna.greene.designs@gmail.com>
2017-11-19 13:45:13 -0700

    initial commit

--- client/src/components/data-components/Data1.js
+++ client/src/components/data-components/Data1.js
@@ -9,6 +9,9 @@ const Data1 = () => {
         <h1>Colorado Population Density</h1>
         <h4>Sourced from the U.S. Census Bureau</h4>
         <img className="data-image" src={require('./images/co-pop-full.png')} alt="pop map" />
+        <div>
+          
+        </div>
       </div>
     </div>
 
