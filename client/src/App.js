6289919859412af8a3fcb0d01617fe2fb3bb485e
Brianna Greene <brianna.greene.designs@gmail.com>
2017-11-19 14:38:03 -0700

    animation working, images need gto be straight

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Header from './components/Header'
 import Dashboard from './components/Dashboard'
-// import LandingPage from './components/LandingPage'
+import LandingPage from './components/LandingPage'
 import Footer from './components/Footer'
 
 
@@ -9,13 +9,33 @@ class App extends Component {
 
   constructor(props) {
     super(props)
-    this.state = { dataDisplay: 1 }
+    this.state = { dataDisplay: 1, mapImageIndex: 0 }
   }
 
+  componentDidMount() {
+    setInterval(this.changeMapImage, 1000)
+  }
+
+  componentWillUnmount(){
+    clearInterval(this.intervalId);
+  }
+
+  // animate colorado population density map
+  changeMapImage = () => {
+    const mapListLength = 4
+    let nextImageIndex = this.state.mapImageIndex + 1
+    if (this.state.mapImageIndex < 3) {
+      this.setState({ mapImageIndex: nextImageIndex })
+    }
+    else {
+      clearInterval(this.intervalId)
+    }
+  }
+
+  // allow drop down on dashboard to display selected data
   updateDataDisplay = (num) => {
     this.setState({ dataDisplay: num })
   }
-
 
 
   render() {
@@ -28,6 +48,7 @@ class App extends Component {
         <Dashboard
           dataDisplay={this.state.dataDisplay}
           updateDataDisplay={this.updateDataDisplay}
+          mapImageIndex={this.state.mapImageIndex}
         />
         <Footer />
       </div>
