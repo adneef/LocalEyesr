68bb5d686d9dd71ec102c533a66ec7e1fe4db00a
Brianna Greene <brianna.greene.designs@gmail.com>
2017-11-20 12:31:40 -0700

    working on pie chart

--- client/src/components/data-components/Data2Chart.js
+++ client/src/components/data-components/Data2Chart.js
@@ -1,17 +1,68 @@
-import React, { Component } from 'react'
+var React = require('react');
+var ReactDOM = require('react-dom');
+var PieChart = require('react-d3-basic').PieChart;
 
 
-class Data2Chart extends Component {
+const Data2Chart = () => {
 
-  
+  (function() {
+    var generalChartData = require('./data.csv')
 
-  render() {
-    return(
-      <div>
-        <button className="btn">Logout</button>
-      </div>
-    )
-  }
+    var width = 700,
+      height = 400,
+      value = function(d) {
+        return +d.population;
+      },
+      name = function(d) {
+        return d.age;
+      },
+      chartSeries = [
+        {
+          "field": "<5",
+          "name": "less than 5"
+        },
+        {
+          "field": "5-13",
+          "name": "5 to 13"
+        },
+        {
+          "field": "14-17",
+          "name": "14 to 17"
+        },
+        {
+          "field": "18-24",
+          "name": "18 to 24"
+        },
+        {
+          "field": "25-44",
+          "name": "25 to 44"
+        },
+        {
+          "field": "45-64",
+          "name": "45 to 64"
+        }
+      ];
+
+      ReactDOM.render(
+        <PieChart
+          data= {generalChartData}
+          width= {width}
+          height= {height}
+          chartSeries= {chartSeries}
+          value = {value}
+          name = {name}
+        />
+      , document.getElementById('data_pie')
+      )
+    })()
+  //
+  // render() {
+  //   return(
+  //     <div>
+  //       <button className="btn">Logout</button>
+  //     </div>
+  //   )
+  // }
 
 }
 
